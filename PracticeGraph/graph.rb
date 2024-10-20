46d4e93a31c54d135dd02564f6b8f78182c558f5
jessec01 <37922492+jessec01@users.noreply.github.com>
2024-10-20 11:01:53 -0400

    Versión 0.7
    solucionado el error para insertar datos en el graph por cuestiones de mala implementación en el apartado de expresión regular, solucionado el error para inicializar el array de array.

--- PracticeGraph/graph.rb
+++ PracticeGraph/graph.rb
@@ -1,20 +1,28 @@
 # Documentacion
 class Graph
   def initialize(num_size)
-    @list_graph=[]
     @num_size=num_size
+    #@num_size.class
+    @list_graph=Array.new(@num_size)
+    make_list_graph
+  end
+  def add_vertices(vertice,edge)
+    #make_list_graph
+    puts vertice
+    puts edge
+    @list_graph[vertice] << edge
+    @list_graph.inspect
+    is_dirigide=0
+    if is_dirigide==1
+      #@list_graph.insert(edge,vertice)
+    end
   end
   def make_list_graph
-    list_aux=[]
-    size.downto(0) do |i|
-      
-      
-      @list_graph<<list_aux
+    @num_size.times do |i|
+      @list_graph[i]=Array.new(0)
     end
-    
-
+    #puts @list_graph.inspect
   end
-
   #getter 
   def  list_graph
     @list_graph
--- PracticeGraph/read.rb
+++ PracticeGraph/read.rb
@@ -3,8 +3,10 @@ require_relative './graph'
 #
 class Read 
   def initialize(name_archive)
+    @graph_dirigide=nil
     read_archive(name_archive)
-    #@graph_dirigide=nil
+    puts @graph_dirigide.list_graph.inspect
+
   end
   def read_archive(name_archive)
     
@@ -18,14 +20,18 @@ class Read
         content.each_line do |linea|
           if num_accountant ==0
             num_size=/\d/.match(linea)
+            @graph_dirigide=Graph.new(num_size[0].to_i)
             #puts num_size
         
           else
-            string_date=/\d\s\d/.match(linea)
-            num_vertices_initial=string_date[0]
-            num_vertices_end=string_date[3]
-            puts num_vertices_end
-            puts num_vertices_initial
+            num_vertices_initial=/\d/.match(linea).to_s
+            #um_vertices_end=string_date
+            num_vertices_end=/(?<=\d| )\d/.match(linea).to_s
+            #puts num_vertices_end.class
+            #puts num_vertices_initial.class
+            @graph_dirigide.add_vertices(num_vertices_initial.to_i,num_vertices_end.to_i)
+            #puts num_vertices_end
+            #puts num_vertices_initial
             
           end
           
@@ -43,6 +49,9 @@ class Read
       puts "Error al intentar abrir el archivo"       
     end
 
+  end
+  def graph_dirigide
+    @graph_dirigide
   end
 end
 
