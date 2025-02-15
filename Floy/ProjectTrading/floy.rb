66ab0ec860a0e84641858864a0944748b5684de1
jessec01 <jegro17@gmail.com>
2025-02-14 21:54:47 -0400

    ProjectTrading: Version:0.2Feature:evitar que se repite comprobaciones en cruz en la matriz

--- Floy/ProjectTrading/floy.rb
+++ Floy/ProjectTrading/floy.rb
@@ -10,19 +10,36 @@ class Floydwarshall
       @pi[num]=Array.new(g.size_graph,nil)
       @ctran[num]=Array.new(g.size_graph,false)
       g.adjacency(num).each do |edge|
-        value=-1*Math.log(edge.size_edge)
-        @mdist[edge.origin][edge.destination]=value.round(1)
-        @pi[edge.origin][edge.destination]=num
-        @ctran[edge.origin][edge.destination]=true
+        value = -Math.log(edge.size_edge)
+        #puts "Después de log: #{value}"
+        #if value>0
+          #value = value * -1.0
+        #end
+        #puts "Después de multiplicar por -1: #{value}"
+        value = value.round(3)
+        #puts "Después de round: #{value}"
+        @mdist[edge.origin][edge.destination] = value
+        @pi[edge.origin][edge.destination] = num
+        @ctran[edge.origin][edge.destination] = true
       end
     end  
+    #see_msdist
     @mdist.size.times do |k|
       @mdist.size.times do |i|
         @mdist.size.times do |j|
-          if @mdist[i][j]>@mdist[i][k]+@mdist[k][j]
-            @mdist[i][j]=@mdist[i][k]+@mdist[k][j]
-            @pi[i][j]=@pi[k][j]
+          if @mdist[i][j]!=@mdist[i][k] and @mdist[i][j]!=@mdist[k][j]
+            if @mdist[i][j]>@mdist[i][k]+@mdist[k][j]
+              @mdist[i][j]=@mdist[i][k]+@mdist[k][j]
+              @pi[i][j]=@pi[k][j]
+            end
+          else
+            puts "@mdist[#{i}][#{j}]>@mdist[#{i}][#{k}]+@mdist[#{k}][#{j}]"
           end
+          #puts "@mdist[#{i}][#{j}]>@mdist[#{i}][#{k}]+@mdist[#{k}][#{j}]"
+          #if k<2
+            #puts "@mdist[#{i}][#{j}]>@mdist[#{i}][#{k}]+@mdist[#{k}][#{j}]"
+            #see_msdist
+          #end
           @ctran[i][j]=@ctran[i][j]|| (@ctran[i][k] && @ctran[k][j])
         end  
       end
@@ -32,7 +49,7 @@ class Floydwarshall
   def see_msdist
     @mdist.size.times do |k|
       @mdist.size.times do |i|
-        print "#{@pi[k][i]}..  "
+        print "#{@mdist[k][i].round(3)}..  "
       end
       puts " "
     end
@@ -41,7 +58,7 @@ class Floydwarshall
   def see_pi
     @mdist.size.times do |k|
       @mdist.size.times do |i|
-        print "#{@mdist[k][i].round(1)}--  "
+        print "#{@pi[k][i]}"
       end
       puts " "
     end
